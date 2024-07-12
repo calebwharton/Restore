8eb48a803392fd5297eba9af0eb7abb5aa46dd46
justin <jhua515@aucklanduni.ac.nz>
2024-07-13 00:58:55 +1200

    Create skeleton profile and sell pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,7 @@ import Login from "@pages/Login";
 import Store from "@pages/Store";
 import SignUp from "@pages/SignUp";
 import About from "@pages/About";
+import Profile from "@pages/Profile";
 
 const router = createBrowserRouter([
     {
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
     {
         path: "/store",
         element: <Store />,
+    },
+    {
+        path: "/profile",
+        element: <Profile />,
     },
     {
         path: "/about",

e37424c2af2b6dee1e66c1289c3d50ce897d12aa
Caleb Wharton <calebwharton123@gmail.com>
2024-07-12 20:51:20 +1200

    set up schema

--- api/db/User.ts
+++ api/db/User.ts
@@ -10,6 +10,15 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  itemsList: {
+    type: Array,
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+  
 });
 
 const User = model('User', userSchema);

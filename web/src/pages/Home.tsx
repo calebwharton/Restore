b94c33454da59c73d91a0b88110853d59f393e21
justin <jhua515@aucklanduni.ac.nz>
2024-07-13 11:57:35 +1200

    Integrate google maps api

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,14 +1,24 @@
-import Banner from "@components/Banner";
-import HowReStoreWorks from "@components/HowReStoreWorks";
+// import Banner from "@components/Banner";
+// import HowReStoreWorks from "@components/HowReStoreWorks";
 import NavBar from "@components/NavBar";
 import React from "react";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 const Home: React.FC = () => {
     return (
         <div>
             <NavBar />
-            <Banner />
-            <HowReStoreWorks />
+            {/* <Banner />
+            <HowReStoreWorks /> */}
+            <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+                <Map
+                    style={{ width: "100vw", height: "100vh" }}
+                    defaultCenter={{ lat: 22.54992, lng: 0 }}
+                    defaultZoom={3}
+                    gestureHandling={"greedy"}
+                    disableDefaultUI={true}
+                />
+            </APIProvider>
 
             <div className="left-items">
                 <div className="our-story">

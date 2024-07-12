3a14d66f47bf473270d0f620193cfc34bf1aa8fe
Nancy Wei <nnwi818@aucklanduni.ac.nz>
2024-07-12 14:21:24 +1200

    change home to landing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { useState } from 'react';
-import Home from '@pages/Home';
+import Landing from '@pages/Landing';
 import Form from '@pages/Form';
 import AboutUs from '@pages/AboutUs';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <Landing />,
   },
   {
     path: '/sign-in',

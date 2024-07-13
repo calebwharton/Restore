6e2b2b48c83c7f4e2545b7aa7128224fa220fda9
justin <jhua515@aucklanduni.ac.nz>
2024-07-13 14:16:42 +1200

    Create footer

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export default function NavBar() {
     return (
         <div className="bg-gray-800 py-6">
             <nav className="container mx-auto flex justify-between items-center">
                 <div className="flex items-center space-x-8">
-                    <Link to="/" className="text-offwhite font-title text-4xl px-6">RESTORE</Link>
-                    <Link to="/about" className="text-offwhite text-lg px-6">ABOUT</Link>
-                    <Link to="/social" className="text-offwhite text-lg px-6">SOCIAL</Link>
+                    <Link
+                        to="/"
+                        className="text-offwhite font-title text-4xl px-6"
+                    >
+                        RESTORE
+                    </Link>
+                    <Link to="/about" className="text-offwhite text-lg px-6">
+                        ABOUT
+                    </Link>
+                    <Link to="/social" className="text-offwhite text-lg px-6">
+                        CREATE
+                    </Link>
+                    <Link to="/social" className="text-offwhite text-lg px-6">
+                        LEADERBOARD
+                    </Link>
                 </div>
                 <div>
                     <Link to="/profile" className="text-offwhite">

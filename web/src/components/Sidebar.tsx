c74e2727111a968f1bb2a1920dddb4611a50bee0
justin <jhua515@aucklanduni.ac.nz>
2024-07-13 18:41:35 +1200

    Change sidebar

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import "../styles/Sidebar.css"
+import React, { useState } from "react";
+import "../styles/Sidebar.css";
 
 interface SidebarProps {
     selectedMarker: string | null;
@@ -7,50 +7,127 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ selectedMarker }) => {
     const [isCreatingEvent, setIsCreatingEvent] = useState(false);
-    const [isInterested, setIsInterested] = useState(false);
-    const [eventDate, setEventDate] = useState('');
+    // const [isInterested, setIsInterested] = useState(false);
+    const [selectedEvent, setSelectedEvent] = useState("");
+    const [eventDate, setEventDate] = useState("");
 
     const handleCreateEvent = () => {
         setIsCreatingEvent(true);
     };
 
     const handleSaveEvent = () => {
-        // Save event logic 
+        // Save event logic
         console.log(`Event created for ${selectedMarker} on ${eventDate}`);
-        setIsCreatingEvent(false); 
+        setIsCreatingEvent(false);
     };
+
+    function goBack() {
+        setIsCreatingEvent(false);
+    }
+
+    function handleClickEvent() {
+        setSelectedEvent("Event");
+    }
+    function goBackFromEvent() {
+        setSelectedEvent("");
+    }
 
     return (
         <div className="sidebar">
             {selectedMarker ? (
                 isCreatingEvent ? (
-                    <div className="event-form">
-                        <h2>Create Event for {selectedMarker}</h2>
+                    <div className="flex flex-col h-full w-full text-left">
+                        <button onClick={goBack} className="font-bold ml-auto">
+                            Back
+                        </button>
+                        <h2 className="text-xl font-bold ml-2">
+                            Create Event for {selectedMarker}
+                        </h2>
+                        <h2 className="text-xl font-bold mt-4 ml-2">TITLE</h2>
                         <input
-                            type="date"
+                            type="text"
+                            className="w-full bg-primary py-2 px-4 rounded-xl"
                             value={eventDate}
                             onChange={(e) => setEventDate(e.target.value)}
                         />
-                        <button onClick={handleSaveEvent}>Save Event</button>
+                        <h2 className="text-xl font-bold mt-4 ml-2">DATE</h2>
+                        <input
+                            type="date"
+                            className="w-full bg-primary py-2 px-4 rounded-xl"
+                            value={eventDate}
+                            onChange={(e) => setEventDate(e.target.value)}
+                        />
+                        <h2 className="text-xl font-bold mt-4 ml-2">
+                            DESCRIPTION
+                        </h2>
+                        <textarea
+                            className="w-full bg-primary py-2 px-4 rounded-xl"
+                            value={eventDate}
+                            onChange={(e) => setEventDate(e.target.value)}
+                        />
+                        <button
+                            className="bg-navy text-primary font-semibold text-xl w-full rounded-xl py-3 mt-auto"
+                            onClick={handleSaveEvent}
+                        >
+                            POST
+                        </button>
+                    </div>
+                ) : selectedEvent ? (
+                    <div className="flex flex-col h-full text-left">
+                        <button
+                            onClick={goBackFromEvent}
+                            className="font-bold ml-auto"
+                        >
+                            Back
+                        </button>
+                        EVENT
+                        <button
+                            className="bg-navy text-primary font-semibold text-xl w-full rounded-xl py-3 mt-auto"
+                            onClick={handleCreateEvent}
+                        >
+                            INTERESTED
+                        </button>
                     </div>
                 ) : (
-                    <>
-                        <h2 className="beach-name">{selectedMarker}</h2>
-                        <hr className="title-line" />
-                        <button
-                            className={`interest-button ${isInterested ? 'active' : ''}`}
-                            onClick={() => setIsInterested(!isInterested)}
+                    <div className="flex flex-col h-full text-left">
+                        <h1 className="text-2xl font-bold mb-6">
+                            {selectedMarker}
+                        </h1>
+                        <div
+                            className="text-left bg-primary mb-2 p-4 rounded-xl font-semibold  hover:cursor-pointer"
+                            onClick={handleClickEvent}
                         >
-                            {isInterested ? 'Interested' : 'Not Interested'}
+                            <h2 className="text-xl font-bold">
+                                {selectedMarker}
+                            </h2>
+                            <p>Location</p>
+                            <p>Date</p>
+                        </div>
+                        <div
+                            className="text-left bg-primary p-4 rounded-xl font-semibold hover:cursor-pointer"
+                            onClick={handleClickEvent}
+                        >
+                            <h2 className="text-xl font-bold">
+                                {selectedMarker}
+                            </h2>
+                            <p>Location</p>
+                            <p>Date</p>
+                        </div>
+
+                        <button
+                            className="bg-navy text-primary font-semibold text-xl w-full rounded-xl py-3 mt-auto"
+                            onClick={handleCreateEvent}
+                        >
+                            CREATE
                         </button>
-                        <button className="create-event-button" onClick={handleCreateEvent}>
-                            Create Event
-                        </button>
-                    </>
+                    </div>
                 )
             ) : (
-                <div className="welcome-message">
-                    <h2>Welcome to ReStore! Please select a beach to see more.</h2>
+                <div className="flex h-full welcome-message">
+                    <div className="bg-primary px-6 py-12 my-auto rounded-xl">
+                        <h2 className="font-title">Welcome to ReSTORE</h2>
+                        <p className="text-sm">Reuse, Reduce, ReStore</p>
+                    </div>
                 </div>
             )}
         </div>

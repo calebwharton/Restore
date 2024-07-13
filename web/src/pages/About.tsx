71aff1417355d074dbc5ae2d7c57956f669928f4
justin <jhua515@aucklanduni.ac.nz>
2024-07-13 15:13:20 +1200

    Adjust about content

--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles/AboutUs.css";
 import NavBar from "@components/NavBar";
 import Footer from "@components/Footer";
+import Trust from "../assets/trust.png";
 
 const About: React.FC = () => {
     return (
@@ -11,15 +12,16 @@ const About: React.FC = () => {
                 <h1 className="font-title text-4xl text-navy">OUR GOAL</h1>
                 <div className="border-t-4 w-full mb-10 border-navy rounded-xl" />
                 <div className="grid grid-cols-2">
-                    <div className="text-navy text-xl overflow-hidden">
+                    <div className="text-navy text-2xl overflow-hidden">
                         <p>
-                            At ReStore, our mission is to transform shopping
-                            into a platform for environmental sustainability.
-                            Our online marketplace not only rehomes pre-loved
-                            items but also supports charitable causes.
+                            Restore is a dedicated social network for beach
+                            cleanups. Restore is designed to empower communities
+                            by making it easier for people to create, join, and
+                            promote cleanup events at their local beaches,
+                            fostering real change in local communities.
                         </p>
                         <br />
-                        <p>
+                        {/* <p>
                             When you shop with us, the proceeds from every sale
                             go directly to supporting charities. Sellers are not
                             just decluttering- they're actively contributing to
@@ -28,12 +30,15 @@ const About: React.FC = () => {
                             future purchases, making sustainable shopping even
                             more rewarding.
                         </p>
-                        <br />
+                        <br /> */}
                         <p>
                             Join us in making a difference - one item at a time.
                         </p>
                     </div>
-                    <div>PLACEHOLDER</div>
+                    <div className="mx-auto">
+                        {/* CHANGE THIS */}
+                        <img src={Trust} alt="" />
+                    </div>
                 </div>
             </div>
             <Footer />

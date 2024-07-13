86ac92fc8aabbcacc6c3efc3847d5539cc7337b6
justin <jhua515@aucklanduni.ac.nz>
2024-07-13 16:13:27 +1200

    Fix login functionality

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import GoogleSigninBtn from "@components/sign-in-button";
+// import GoogleSigninBtn from "@components/sign-in-button";
 import "../styles/login.css";
+import axios from "axios";
 
-function Login() {
+interface LoginProps {}
+
+const Login: React.FC<LoginProps> = () => {
+    // const history = useHistory();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
     const navigate = useNavigate();
-    const handleSignInClick = () => {
-        navigate("/sign-in");
+
+    const handleSignIn = async () => {
+        if (username && password) {
+            try {
+                await axios.post(
+                    `${import.meta.env.VITE_SERVER_URL}/api/user/check-user`,
+                    {
+                        email: username,
+                        password: password,
+                    }
+                );
+                navigate("/");
+            } catch (error) {
+                console.error("Error submitting form:", error);
+                // setValidSubmit("error");
+            }
+        } else {
+            console.log("no details");
+            // setValidSubmit("error");
+        }
     };
 
     return (
@@ -22,24 +49,35 @@ function Login() {
                         type="text"
                         placeholder="Username"
                         className="input-field"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
                     />
                     <input
                         type="password"
                         placeholder="Password"
                         className="input-field"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
+                    {error && <p className="text-red-500">{error}</p>}
+                    <button
+                        onClick={handleSignIn}
+                        className="ml-auto bg-navy text-primary font-semibold px-6 py-2 rounded-xl"
+                    >
+                        SIGN IN
+                    </button>
                 </div>
-                <p className="mb-6">
-                    Dont have an account?{" "}
+                <p className="mt-6">
+                    Don't have an account?{" "}
                     <Link className="underline" to="/signup">
                         SignUp
                     </Link>
                 </p>
-                <GoogleSigninBtn onClick={handleSignInClick} />
+                {/* <GoogleSigninBtn /> */}
             </div>
             <div className="admin-login-right-items bg-navy h-screen"></div>
         </div>
     );
-}
+};
 
 export default Login;

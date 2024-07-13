16579fdf6ac1cad134b985c8d5d63748310c42da
justin <jhua515@aucklanduni.ac.nz>
2024-07-13 16:19:57 +1200

    Store user id in local storage and display error messages

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -17,21 +17,23 @@ const Login: React.FC<LoginProps> = () => {
     const handleSignIn = async () => {
         if (username && password) {
             try {
-                await axios.post(
+                const response = await axios.post(
                     `${import.meta.env.VITE_SERVER_URL}/api/user/check-user`,
                     {
                         email: username,
                         password: password,
                     }
                 );
+                console.log(response.data);
+                localStorage.setItem("user_id", response.data._id);
                 navigate("/");
             } catch (error) {
                 console.error("Error submitting form:", error);
-                // setValidSubmit("error");
+                setError("Invalid email or password");
             }
         } else {
             console.log("no details");
-            // setValidSubmit("error");
+            setError("Please enter a valid email and password");
         }
     };
 
@@ -55,11 +57,15 @@ const Login: React.FC<LoginProps> = () => {
                     <input
                         type="password"
                         placeholder="Password"
-                        className="input-field"
+                        className="input-field mb-0"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                    {error && <p className="text-red-500">{error}</p>}
+                    {error && (
+                        <p className="text-red-500 text-sm mb-4 ml-2">
+                            {error}
+                        </p>
+                    )}
                     <button
                         onClick={handleSignIn}
                         className="ml-auto bg-navy text-primary font-semibold px-6 py-2 rounded-xl"

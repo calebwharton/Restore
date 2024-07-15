03cf138ecc151f965a9dc564ef5ca4423c444a17
justin <jhua515@aucklanduni.ac.nz>
2024-07-15 14:39:10 +1200

    Resolve footer causing horizontal scroll

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import footerGraphic from "../assets/footer.svg";
 const Footer = () => {
     return (
         <div className="relative mt-auto">
-            <div className="relative w-screen">
+            <div className="relative w-full">
                 <img src={footerGraphic} className="w-full" alt="" />
                 <div className="absolute inset-0 flex items-end justify-center pb-4">
                     <p className="text-navy">
